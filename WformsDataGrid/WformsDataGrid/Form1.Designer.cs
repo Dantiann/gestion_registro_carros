cd5c288647e9a76dbff6a328542c0a0a35fa7695
Alexander Garnica <131830717+Dantiann@users.noreply.github.com>
2024-09-01 15:46:13 -0500

    Add files via upload

---
+++ WformsDataGrid/WformsDataGrid/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace WformsDataGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.frmVehiculos = new System.Windows.Forms.Label();
+            this.lblPlaca = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.lblModelo = new System.Windows.Forms.Label();
+            this.txtPlaca = new System.Windows.Forms.TextBox();
+            this.txtTipo = new System.Windows.Forms.TextBox();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.dgvVehiculos = new System.Windows.Forms.DataGridView();
+            this.colPlaca = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colModelo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblTituloVehiculo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVehiculos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // frmVehiculos
+            // 
+            this.frmVehiculos.AutoSize = true;
+            this.frmVehiculos.Location = new System.Drawing.Point(315, 41);
+            this.frmVehiculos.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.frmVehiculos.Name = "frmVehiculos";
+            this.frmVehiculos.Size = new System.Drawing.Size(0, 16);
+            this.frmVehiculos.TabIndex = 0;
+            // 
+            // lblPlaca
+            // 
+            this.lblPlaca.AutoSize = true;
+            this.lblPlaca.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlaca.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblPlaca.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlaca.ForeColor = System.Drawing.Color.Transparent;
+            this.lblPlaca.Location = new System.Drawing.Point(34, 30);
+            this.lblPlaca.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPlaca.Name = "lblPlaca";
+            this.lblPlaca.Size = new System.Drawing.Size(84, 32);
+            this.lblPlaca.TabIndex = 1;
+            this.lblPlaca.Text = "Placa";
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.BackColor = System.Drawing.Color.Transparent;
+            this.lblTipo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblTipo.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipo.ForeColor = System.Drawing.Color.Transparent;
+            this.lblTipo.Location = new System.Drawing.Point(34, 85);
+            this.lblTipo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(70, 32);
+            this.lblTipo.TabIndex = 2;
+            this.lblTipo.Text = "Tipo";
+            // 
+            // lblModelo
+            // 
+            this.lblModelo.AutoSize = true;
+            this.lblModelo.BackColor = System.Drawing.Color.Transparent;
+            this.lblModelo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblModelo.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblModelo.ForeColor = System.Drawing.Color.Transparent;
+            this.lblModelo.Location = new System.Drawing.Point(34, 141);
+            this.lblModelo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblModelo.Name = "lblModelo";
+            this.lblModelo.Size = new System.Drawing.Size(98, 32);
+            this.lblModelo.TabIndex = 3;
+            this.lblModelo.Text = "Modelo";
+            // 
+            // txtPlaca
+            // 
+            this.txtPlaca.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPlaca.Font = new System.Drawing.Font("Cascadia Mono", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlaca.ForeColor = System.Drawing.Color.DarkBlue;
+            this.txtPlaca.Location = new System.Drawing.Point(159, 33);
+            this.txtPlaca.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPlaca.Name = "txtPlaca";
+            this.txtPlaca.Size = new System.Drawing.Size(185, 28);
+            this.txtPlaca.TabIndex = 4;
+            // 
+            // txtTipo
+            // 
+            this.txtTipo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTipo.Font = new System.Drawing.Font("Cascadia Mono", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTipo.ForeColor = System.Drawing.Color.DarkBlue;
+            this.txtTipo.Location = new System.Drawing.Point(159, 87);
+            this.txtTipo.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTipo.Name = "txtTipo";
+            this.txtTipo.Size = new System.Drawing.Size(185, 28);
+            this.txtTipo.TabIndex = 5;
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtModelo.Font = new System.Drawing.Font("Cascadia Mono", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtModelo.ForeColor = System.Drawing.Color.DarkBlue;
+            this.txtModelo.Location = new System.Drawing.Point(159, 142);
+            this.txtModelo.Margin = new System.Windows.Forms.Padding(4);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(185, 28);
+            this.txtModelo.TabIndex = 6;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnAgregar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnAgregar.Font = new System.Drawing.Font("Cascadia Mono SemiLight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.ForeColor = System.Drawing.Color.PaleGreen;
+            this.btnAgregar.Location = new System.Drawing.Point(431, 41);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(177, 47);
+            this.btnAgregar.TabIndex = 7;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnLimpiar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnLimpiar.Font = new System.Drawing.Font("Cascadia Mono SemiLight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.ForeColor = System.Drawing.Color.PaleGreen;
+            this.btnLimpiar.Location = new System.Drawing.Point(431, 109);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(177, 47);
+            this.btnLimpiar.TabIndex = 8;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // dgvVehiculos
+            // 
+            this.dgvVehiculos.AllowUserToOrderColumns = true;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Cascadia Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Teal;
+            this.dgvVehiculos.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvVehiculos.BackgroundColor = System.Drawing.Color.DimGray;
+            this.dgvVehiculos.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.RaisedVertical;
+            this.dgvVehiculos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVehiculos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colPlaca,
+            this.colTipo,
+            this.colModelo});
+            this.dgvVehiculos.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dgvVehiculos.Location = new System.Drawing.Point(1048, 66);
+            this.dgvVehiculos.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvVehiculos.Name = "dgvVehiculos";
+            this.dgvVehiculos.RowHeadersWidth = 51;
+            this.dgvVehiculos.Size = new System.Drawing.Size(506, 201);
+            this.dgvVehiculos.TabIndex = 9;
+            this.dgvVehiculos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvVehiculos_CellContentClick);
+            // 
+            // colPlaca
+            // 
+            this.colPlaca.HeaderText = "Placa";
+            this.colPlaca.MinimumWidth = 6;
+            this.colPlaca.Name = "colPlaca";
+            this.colPlaca.Width = 125;
+            // 
+            // colTipo
+            // 
+            this.colTipo.HeaderText = "Tipo";
+            this.colTipo.MinimumWidth = 6;
+            this.colTipo.Name = "colTipo";
+            this.colTipo.Width = 125;
+            // 
+            // colModelo
+            // 
+            this.colModelo.HeaderText = "Modelo";
+            this.colModelo.MinimumWidth = 6;
+            this.colModelo.Name = "colModelo";
+            this.colModelo.Width = 125;
+            // 
+            // lblTituloVehiculo
+            // 
+            this.lblTituloVehiculo.AutoSize = true;
+            this.lblTituloVehiculo.BackColor = System.Drawing.Color.Black;
+            this.lblTituloVehiculo.Font = new System.Drawing.Font("Impact", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTituloVehiculo.ForeColor = System.Drawing.Color.YellowGreen;
+            this.lblTituloVehiculo.Location = new System.Drawing.Point(1104, 13);
+            this.lblTituloVehiculo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTituloVehiculo.Name = "lblTituloVehiculo";
+            this.lblTituloVehiculo.Size = new System.Drawing.Size(379, 48);
+            this.lblTituloVehiculo.TabIndex = 10;
+            this.lblTituloVehiculo.Text = "GESTIÓN DE VEHÍCULOS";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1584, 841);
+            this.Controls.Add(this.lblTituloVehiculo);
+            this.Controls.Add(this.dgvVehiculos);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtModelo);
+            this.Controls.Add(this.txtTipo);
+            this.Controls.Add(this.txtPlaca);
+            this.Controls.Add(this.lblModelo);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.lblPlaca);
+            this.Controls.Add(this.frmVehiculos);
+            this.ForeColor = System.Drawing.Color.Cornsilk;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Vehiculos";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVehiculos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label frmVehiculos;
+        private System.Windows.Forms.Label lblPlaca;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.Label lblModelo;
+        private System.Windows.Forms.TextBox txtPlaca;
+        private System.Windows.Forms.TextBox txtTipo;
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.DataGridView dgvVehiculos;
+        private System.Windows.Forms.Label lblTituloVehiculo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPlaca;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTipo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colModelo;
+    }
+}
+
---
+++ WformsDataGrid/WformsDataGrid/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WformsDataGrid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string placa, tipo, modelo;
+
+            placa = txtPlaca.Text;
+            tipo = txtTipo.Text;
+            modelo = txtModelo.Text;
+            
+            dgvVehiculos.Rows.Add(placa, tipo, modelo);
+            
+            txtModelo.Text = "";
+            txtPlaca.Text = "";
+            txtTipo.Text = "";
+                     
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            dgvVehiculos.Rows.Clear();
+        }
+
+        private void dgvVehiculos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
